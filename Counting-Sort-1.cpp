4f2985ea7727efd71024f38ef365ad256b8458bf
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-07-31 09:56:11 +0900

    Update Counting-Sort-1.cpp

--- Counting-Sort-1.cpp
+++ Counting-Sort-1.cpp
@@ -1,3 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'countingSort' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
 vector<int> countingSort(vector<int> arr)
 {
     vector<int> count(100);
