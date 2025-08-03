61f7dc44aa5ab1677f93638a652860fda456b21c
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-08-04 06:27:56 +0900

    Update Counting-Sort-1.cs

--- Counting-Sort-1.cs
+++ Counting-Sort-1.cs
@@ -11,3 +11,24 @@ using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using System.Text;
 using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        List<int> result = Result.countingSort(arr);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
