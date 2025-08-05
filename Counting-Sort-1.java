2285ef691f508ccb3143d4b19231f6da22df9622
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-08-06 05:15:41 +0900

    Update Counting-Sort-1.java

--- Counting-Sort-1.java
+++ Counting-Sort-1.java
@@ -1,1 +1,32 @@
-Counting-Sort-1.java
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'countingSort' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<Integer> countingSort(List<Integer> arr) {
+        List<Integer> count = new ArrayList<>(Collections.nCopies(100, 0));
+
+        for (int n : arr) {
+            count.set(n, count.get(n) + 1);
+        }
+
+        return count;
+    }
+}
+
